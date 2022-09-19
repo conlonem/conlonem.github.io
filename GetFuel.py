6d85fe3fa1474257e95a3a7989c3e4adb302ad90
Em Conlon <66461347+conlonem@users.noreply.github.com>
2022-09-19 22:57:03 +1000

    new graph html

--- GetFuel.py
+++ GetFuel.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 data = pd.read_excel('price_history_checks_august2022.xlsx')
 data=data.loc[data['FuelCode'] == 'E10']
@@ -10,6 +11,7 @@ data['PriceDate']=data['PriceUpdatedDate'].dt.date
 data = data[data['PriceUpdatedDate'].dt.strftime('%Y-%m-%d') == "2022-01-08"]
 data = data.loc[(data['Postcode'] >= 2287) & (data['Postcode'] <= 2322)]
 data=data.dropna()
-print(data)
-data.plot(x ='ServiceStationName', y='Price', kind='bar')
-plt.show()
+#data.plot(x ='ServiceStationName', y='Price', kind='bar')
+data = px.bar(data, x ='ServiceStationName', y='Price')
+data.write_html("index1.html")
+#plt.show()
