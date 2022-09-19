99d3eb6af21b6977f9a1d3f26afa74a755fdb882
Em Conlon <66461347+conlonem@users.noreply.github.com>
2022-09-19 23:16:40 +1000

    changes

--- GetFuel.py
+++ GetFuel.py
@@ -12,6 +12,6 @@ data = data[data['PriceUpdatedDate'].dt.strftime('%Y-%m-%d') == "2022-01-08"]
 data = data.loc[(data['Postcode'] >= 2287) & (data['Postcode'] <= 2322)]
 data=data.dropna()
 #data.plot(x ='ServiceStationName', y='Price', kind='bar')
-data = px.line(data, x ='ServiceStationName', y='Price')
+data = px.bar(data, x ='ServiceStationName', y='Price')
 data.write_html("index.html")
 #plt.show()
